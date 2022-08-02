6a9f0026002c9bfbf08a2c4c9518d07e8e02cff8
schectman <schectman@google.com>
2022-08-02 18:03:21 +0000

    Add Linux support

--- example/linux/flutter/generated_plugin_registrant.cc
+++ example/linux/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,10 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <sw_rend/sw_rend_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) sw_rend_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "SwRendPlugin");
+  sw_rend_plugin_register_with_registrar(sw_rend_registrar);
 }
